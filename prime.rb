23d34060463a62a3046b292537f78d7560cd72a8
Maija Barrins <maija.barrins@gmail.com>
2019-04-25 20:24:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2
+
+  count = 2
+  while count <= Math.sqrt(number).floor
+    return false if number % count == 0
+    count += 1
+  end
+  true
+end
