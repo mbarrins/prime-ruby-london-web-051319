0269c2db5cb46b46d1c34eeda5139e97646882b7
Maija Barrins <maija.barrins@gmail.com>
2019-04-25 20:13:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 def prime?(number)
+  if number < 1
+    return false
+  end
+  
   count = 2
   while count < Math.sqrt(number).floor
     if number % count == 0
       return false
     end
-    puts count
     count += 1
   end
   true
